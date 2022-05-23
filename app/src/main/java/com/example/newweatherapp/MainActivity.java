cc176bc612b918de1537a6acdb0c3b86aebfb622
Linhh4869 <72199302+Linhh4869@users.noreply.github.com>
2022-05-23 14:37:26 +0700

    am

---
+++ app/src/main/java/com/example/newweatherapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.newweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView abc1;
+    private TextView abc2;
+    private TextView abc3;
+    private TextView abc4;
+    private TextView abc5;
+    private TextView abc6;
+    private TextView abc7;
+    private TextView abc8;
+    private TextView abc9;
+    private TextView abc10;
+    private TextView abc11;
+    ImageView IconWeather;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+/*        abc8 = findViewById(R.id.Pressure);
+        abc8.setText("abc8");
+
+        abc9 = findViewById(R.id.Visiabc1 = findViewById(R.id.weather);
+        abc1.setText("abc1");
+
+        abc2 = findViewById(R.id.air);
+        abc2.setText("abc2");
+
+        abc3 = findViewById(R.id.temp);
+        abc3.setText("abc3");
+
+        abc4 = findViewById(R.id.feelTemp);
+        abc4.setText("abc4");
+
+        abc5 = findViewById(R.id.sideNotice);
+        abc5.setText("abc5");
+
+        abc11 = findViewById(R.id.Wind);
+        abc11.setText("abc11");
+
+        abc6 = findViewById(R.id.Humid);
+        abc6.setText("abc6");
+
+        abc7 = findViewById(R.id.UV);
+        abc7.setText("abc7");
+        abc9.setText("abc9");
+
+        abc10 = findViewById(R.id.DevPoint);
+        abc10.setText("abc10");   */
+    }
+}
